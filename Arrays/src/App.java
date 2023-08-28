62a85ad1234b2e7737a36120ee166ee2d8d1167b
neonguyen-dev <neo20030125@gmail.com>
2023-08-28 21:56:42 +0200

    Initial commit of first assigment

--- Arrays/src/App.java
+++ Arrays/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Random;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] given = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+        Random rnd = new Random();
+        int sum = 0;
+
+        long t0 = System.nanoTime();
+        for (int i = 0; i < 1000; i++) {
+            sum += given[rnd.nextInt(10)];
+        }
+        long t1 = System.nanoTime();
+        System.out.println(" one read operation in " + (t1 - t0) / 1000 + " nanoseconds");
     }
 }
