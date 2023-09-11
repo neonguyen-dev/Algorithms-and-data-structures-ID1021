71ff1b0a62a7aaa9b9f6485cdbc26d1de4b0fdbf
neonguyen-dev <neo20030125@gmail.com>
2023-09-11 11:47:21 +0200

    insertion sort is finished

--- Sorting/src/App.java
+++ Sorting/src/App.java
@@ -13,7 +13,7 @@ public class App {
         int[] array = new int[size];
 
         for (int i = 0; i < size; i++) {
-            array[i] = rand.nextInt(100000000);
+            array[i] = rand.nextInt(size);
         }
 
         return array;
@@ -23,29 +23,30 @@ public class App {
         for (int i = 0; i < array.length - 1; i++) {
             // let's set the first candidate to the index itself
             int cand = i;
-            for (int j = i; j < array.length; j++) {
+            for (int j = i + 1; j < array.length; j++) {
                 // If the element at position j is smaller than the value
                 // at the candidate position - then you have a new candidate
-                if (array[cand] > array[j]) {
+                if (array[j] < array[cand]) {
                     cand = j;
                 }
             }
             // Swap the item at position i with the item at the candidate position.
+            int temp = array[i];
             array[i] = array[cand];
+            array[cand] = temp;
         }
     }
 
     public static void insertion_sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = i; j > 0 && array[j] > array[i]; j--) {
+            int key = array[i];
+            int j = i - 1;
+            for (; j >= 0 && array[j] > key; j--) {
                 // for each element from i towards 1, swap the item found with the
                 // item before it if it is smaller
-                if(array[j] > array[i]){
-                    int temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
-                }
+                array[j + 1] = array[j];
             }
+            array[j + 1] = key;
         }
     }
 }
